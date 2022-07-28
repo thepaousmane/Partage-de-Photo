4ffe0b91c7df9f68db5c58c843425868f22f0a4f
Ousmane Pathé Dia <hampdia6@gmail..com>
2022-07-29 01:14:00 +1300

    mon premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent,
+    HeaderComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,23 @@
+export class FaceSnap {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  location?: string;
+  
+  // constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
+  //   this.title = title;
+  //   this.description = description;
+  //   this.imageUrl = imageUrl;
+  //   this.createdDate = createdDate;
+  //   this.snaps = snaps;
+  // }
+//raccourcis pour faire tout ça : 
+    // constructor(public title: string,
+    //             public description: string,
+    //             public imageUrl: string,
+    //             public createdDate: Date,
+    //             public snaps: number) {
+    // }
+  }
